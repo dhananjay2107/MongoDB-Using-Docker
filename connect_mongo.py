51a6ed9cc326391f3b7eb2a1ece1bc823e874cbd
dhananjay2107 <dhananjayjshenoy21@gmail.com>
2024-10-02 11:11:29 +0530

    Updated files

--- connect_mongo.py
+++ connect_mongo.py
@@ -5,12 +5,13 @@ insert documents, and fetch documents from the database.
 from pymongo import MongoClient
 import pymongo
 
-def connect_to_db(username, password, host, port, auth_db):
+def connect_to_db(username, password, host, port,db):
     """Connect to MongoDB and return the client instance."""
     try:
         mongo_client = MongoClient(
-            f"mongodb://{username}:{password}@{host}:{port}/?authSource={auth_db}"
+            f"mongodb://{username}:{password}@{host}:{port}/?authSource={db}"
         )
+        print("Connection Successful:")
         return mongo_client
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB:", e)
@@ -19,11 +20,9 @@ def connect_to_db(username, password, host, port, auth_db):
 def create_and_insert(mongo_client, db_name, collection_name, document):
     """Create a database and collection, then insert a document."""
     try:
-
         db = mongo_client[db_name]
         collection = db[collection_name]
-        inserted_id = collection.insert_one(document).inserted_id
-        print(f"Inserted document ID: {inserted_id}")
+        collection.insert_one(document)
     except pymongo.errors.PyMongoError as e:
         print("Error during insert operation:", e)
 
@@ -41,14 +40,14 @@ def fetch_documents(mongo_client, db_name, collection_name):
 if __name__ == "__main__":
     USERNAME = "mongoadmin"
     PASSWORD = "mongodemo"
-    HOST = "localhost"
+    HOST = "mongo-demo"
     PORT = 27017
     AUTH_DB = "admin"
 
     client = connect_to_db(USERNAME, PASSWORD, HOST, PORT, AUTH_DB)
     if client:
         document = {"name": "John", "age": 29, "city": "Kochi"}
-        create_and_insert(client, "mydatabase", "mycollection", document)
-        fetch_documents(client, "mydatabase", "mycollection")
+        create_and_insert(client, "employeedb", "employee", document)
+        fetch_documents(client, "employeedb", "employee")
 
     client.close()
